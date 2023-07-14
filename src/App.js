796d35cd3fc28ff356726502b2cc5e39acaeb675
Moizajmal2456 <moizajmal2456@gmail.com>
2023-07-14 11:52:30 +0500

    Add about me section

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-
-
-import { Education } from "./Components/Education/Education";
-import { Experience } from "./Components/Experience/Experience";
 import Home from "./Components/Home/Home";
+import { About } from "./Components/About/About";
 import { Navbar } from "./Components/Navbar/navbar";
 import { ContactUs } from "./Components/ContactUs/ContactUs";
+import { Education } from "./Components/Education/Education";
+import { Experience } from "./Components/Experience/Experience";
 function App() {
   return (
     <>
     <Navbar/>
     <Home/>
+    <About/>
     <Education/>
     <Experience/>
     <ContactUs/>
---
+++ src/Components/About/About.jsx
@@ -0,0 +1,33 @@
+import style from "./styles.module.scss";
+
+export const About = () => {
+return(
+<div className={style.aboutWrapper}>   
+<div className={style.leftSection}>
+<h1>About Me</h1>
+<p>My name is Moiz Ajmal. I am Mern Stack developer
+    with six months experience. I have good experience in 
+    Frontend development using React Js and i am also good
+    in backend development using Node Js.  
+</p>
+</div>
+<div className={style.rightSection}>
+<h2>HTML & CSS </h2>
+<p>90%</p>
+<div className={style.progressBar1}>
+<div className={style.progress}></div>
+</div>
+<h2>JavaScript </h2>
+<p>80%</p>
+<div className={style.progressBar2}>
+<div className={style.progress}></div>
+</div>
+<h2>React Js </h2>
+<p>60%</p>
+<div className={style.progressBar3}>
+<div className={style.progress}></div>
+</div>
+</div>
+</div>
+);
+} ;
