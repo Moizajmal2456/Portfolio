3910ca0b38299b30d0e40062fb41e66106dc0eac
Moizajmal2456 <moizajmal2456@gmail.com>
2023-07-30 15:05:05 +0500

    Add Matric , Geekybugs and Exd certificate

--- src/Components/Education/Education.jsx
+++ src/Components/Education/Education.jsx
@@ -3,27 +3,6 @@ import { EducationData } from "../../Data/Data";
 import style from "./styles.module.scss";
 import { Cards } from "../Cards/Cards";
 
-const interUrl = "https://drive.google.com/file/d/11p5niiiOl3zUJwvnoRvH9rD36svb0YJU/view?usp=drive_link";
-const bsUrl = "https://drive.google.com/file/d/1t7zF4C4ArNkG8tzgEnx68fxi4AXQVkNl/view?usp=drive_link";
-
-const handleIntermediateResult = () => {
-  const link = document.createElement('a');
-  link.href = interUrl;
-  link.target = '_blank';
-  document.body.appendChild(link);
-  link.click();
-  document.body.removeChild(link);
-};
-
-const handleBsResult = () => {
-  const link = document.createElement('a');
-  link.href = bsUrl;
-  link.target = '_blank';
-  document.body.appendChild(link);
-  link.click();
-  document.body.removeChild(link);
-};
-
 const Education = () => {
 
   const [isVisible, setIsVisible] = useState(false);
