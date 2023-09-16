47c973db895779914e95603efe1bbeb2cf646808
Moizajmal2456 <moizajmal2456@gmail.com>
2023-09-16 23:03:43 +0500

    Make education cards to slider

--- src/Components/Experience/Experience.jsx
+++ src/Components/Experience/Experience.jsx
@@ -1,8 +1,18 @@
 import React, { useEffect, useRef, useState } from "react";
 import style from "./styles.module.scss";
-import { ExperienceData } from "../../Data/Data";
-import { Cards } from "../Cards/Cards";
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
 export const Experience = () => {
+
+  const settings = {
+    dots: true, // Show navigation dots
+    infinite: true, // Loop the carousel
+    speed: 500, // Transition speed in milliseconds
+    slidesToShow: 3, // Number of slides to show at once
+    slidesToScroll: 1, // Number of slides to scroll at a time
+  };
 
   const [isVisible, setIsVisible] = useState(false);
   const componentRef = useRef(null);
@@ -31,16 +41,18 @@ return(
 <div className={style.experienceWrapper} id="experience">
   <h1>Experience</h1>
     <div ref={componentRef}  className={`${style.cardsWrapper} ${isVisible ? style.visible : ""}`}>
-    {ExperienceData.map(data => {
-      return(
-        <Cards
-        class1={data.type}
-        subject={data.technology}
-        institute={data.office}
-        marks={data.experience}
-        buttonText={data.buttonText}/>
-      )})
-    }
+    <Slider {...settings}>
+      <div>
+        <h3>Slide 1</h3>
+        {/* <img src="/Images/School.png" alt="school"/> */}
+      </div>
+      <div>
+        <h3>Slide 2</h3>
+      </div>
+      <div>
+        <h3>Slide 3</h3>
+      </div>
+    </Slider>
     </div>
 </div>    
 );
