dcea124d415faf834a66761408ce03364f43d8d4
Mohammad Abeer <99338487+mohammad-abeer-geekybugs@users.noreply.github.com>
2023-09-20 00:15:31 +0500

    Update Experience.jsx
    React-Slick implementation in Experience Component

--- src/Components/Experience/Experience.jsx
+++ src/Components/Experience/Experience.jsx
@@ -58,3 +58,75 @@ return(
 );
 };
 
+
+
+/* -------------------React Slick Implementation-------------------------- */
+/*
+import React, { useEffect, useRef, useState } from "react";
+import style from "./styles.module.scss";
+import { ExperienceData } from "../../Data/Data";
+import { Cards } from "../Cards/Cards";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+
+const settings = {
+  dots: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 2,
+  slidesToScroll: 1,
+};
+
+export const Experience = () => {
+  const [isVisible, setIsVisible] = useState(false);
+  const componentRef = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          setIsVisible(true);
+        }
+      });
+    });
+
+    if (componentRef.current) {
+      observer.observe(componentRef.current);
+    }
+
+    return () => {
+      if (componentRef.current) {
+        observer.unobserve(componentRef.current);
+      }
+    };
+  }, []);
+
+  return (
+    <div className={style.experienceWrapper} id="experience">
+      <h1>Experience</h1>
+
+      <div ref={componentRef}>
+        <Slider {...settings}>
+          {ExperienceData.map((data) => {
+            return (
+              <div style={{ margin: "auto" }}>
+                <Cards
+                  class1={data.type}
+                  subject={data.technology}
+                  institute={data.office}
+                  marks={data.experience}
+                  buttonText={data.buttonText}
+                />
+              </div>
+            );
+          })}
+        </Slider>
+      </div>
+    </div>
+  );
+};
+*/
+
+
+
